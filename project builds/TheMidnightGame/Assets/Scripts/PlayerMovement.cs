153b36fc6ce7b7273b375dc4cc166bc5d8126655
ben <bmfboom9@gmail.com>
2024-10-30 16:36:34 -0600

    Integrated First-Person Player Movement into DEMO Scene

--- project builds/TheMidnightGame/Assets/Scripts/PlayerMovement.cs
+++ project builds/TheMidnightGame/Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,44 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
 
-    CharacterController characterController;
-    PlayerInput input;
+    CharacterController controller;
+    Vector3 moveDirection;
+    Vector3 movementVector;
+    Transform cameraTransform;
+
 
     [SerializeField] float moveSpeed = 1;
 
-    private void Awake()
+    private void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        cameraTransform = Camera.main.transform;
+    }
+
+    void OnMove(InputValue input)
+    {
+        //updates x and z values of moveDirection vector according to player input
+        Vector2 inputVector = input.Get<Vector2>();
+        moveDirection = new Vector3(inputVector.x, 0, inputVector.y).normalized;
+
+    }
+
     void Update()
     {
+        //adjusts move direction to be relative to the current facing direction (so w always goes forward)
+        //this causes the player to move slower if they are looking up or down, though. (this is fixed below)
+        movementVector = cameraTransform.forward * moveDirection.z + cameraTransform.right * moveDirection.x;
+
+        //removes any vertical influence on the vector so the player moves at full speed
+        movementVector.y = 0;
+        movementVector.Normalize();
+
+        controller.Move(movementVector * moveSpeed * Time.deltaTime);
     }
 }
