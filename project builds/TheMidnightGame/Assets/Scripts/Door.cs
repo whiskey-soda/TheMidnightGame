dfb858e65fecc33c1b624fcc670610195d25b4d6
ben <bmfboom9@gmail.com>
2024-11-19 15:29:54 -0700

    Add door opening/closing on interaction

--- project builds/TheMidnightGame/Assets/Scripts/Door.cs
+++ project builds/TheMidnightGame/Assets/Scripts/Door.cs
@@ -2,9 +2,68 @@ using UnityEngine;
 
 public class Door : InteractableProp
 {
+
+    [SerializeField] float maxOpenRotation = -115;
+    [SerializeField] float rotateSpeed = 150;
+
+    Quaternion closedAngle;
+    Quaternion openAngle;
+
+
+    enum DoorState { open, closed, opening, closing }
+
+    DoorState doorState = DoorState.closed;
+
+    private void Awake()
+    {
+        closedAngle = transform.parent.rotation;
+        openAngle = Quaternion.Euler(transform.parent.rotation.eulerAngles + new Vector3(0, maxOpenRotation, 0));
+    }
+
+    private void Update()
+    {
+
+        if (doorState == DoorState.opening)
+        {
+            transform.parent.rotation = Quaternion.RotateTowards(transform.parent.rotation, openAngle, rotateSpeed * Time.deltaTime);
+
+            if (transform.parent.rotation == openAngle)
+            {
+                doorState = DoorState.open;
+            }
+        }
+
+        if (doorState == DoorState.closing)
+        {
+            transform.parent.rotation = Quaternion.RotateTowards(transform.parent.rotation, closedAngle, rotateSpeed * Time.deltaTime);
+
+            if(transform.parent.rotation == closedAngle)
+            {
+                doorState = DoorState.closed;
+            }
+        }
+    }
+
     public override void ProcessInteraction()
     {
-        Debug.Log("DOOR INTERACTION DETECTED");
+        switch (doorState)
+        {
+            case DoorState.open:
+                doorState = DoorState.closing;
+                break;
+
+            case DoorState.closed:
+                doorState = DoorState.opening;
+                break;
+
+            case DoorState.opening:
+                doorState = DoorState.closing;
+                break;
+
+            case DoorState.closing:
+                doorState = DoorState.opening;
+                break;
+        }
 
     }
 }
